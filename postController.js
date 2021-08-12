1bb569babd2240980f7445549de4a45f8921a0e6
Shyma Grasious <shymaser@gmail.com>
2021-08-12 16:57:06 +0300

    postService

--- postController.js
+++ postController.js
@@ -1,63 +1,54 @@
-import Post from './post.js'
+import PostService from './postService.js'
 class PostController {
     async create(req, res) {
         try {
-            const { author, title, content, picture } = req.body
-            const post = await Post.create({ author, title, content, picture })
+            const post = await PostService.create(req.body)
             res.status(200)
             return res.json(post)
         } catch (error) {
-            res.json(error)
+            res.status(500).json(error)
         }
     }
 
     async getAll(req, res) {
         try {
-            const posts = await Post.find()
+            const posts = await PostService.getAll()
             res.status(200)
             return res.json(posts)
         } catch (error) {
-            res.status(500)
-            res.json(error)
+            res.status(500).json(error)
         }
     }
 
     async getOne(req, res) {
         try {
             const {id} = req.params
-            if (!id) res.status(400).json({message: 'такого поста не существует'})
-            const post = await Post.findById(id)
+            const post = await PostService.getOne(id)
             res.status(200)
             return res.json(post)
         } catch (error) {
-            res.status(500)
-            res.json(error)
+            res.status(500).json(error.message)
         }
     }
 
     async update(req, res) {
         try {
             const post = req.body
-            if (!post._id) res.status(400).json({message: 'такого поста не существует'})
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
+            const updatedPost = await PostService.update(post)
             return res.json(updatedPost)
         } catch (error) {
-            res.status(500)
-            res.json(error)
+            res.status(500).json(error.message)
         }
     }
 
     async delete(req, res) {
         try {
-            const {id} = req.params
-            if (!id) res.status(400).json({message: 'такого поста не существует'})
-            const post = await Post.findByIdAndDelete(id)
+            const post = await PostService.delete(req.params.id)
             return res.json(post)
         } catch (error) {
-            res.status(500)
-            res.json(error)
+            res.status(500).json(error.message)
         }
     }
 }
 
-export default new PostController
+export default new PostController()
--- router.js
+++ router.js
@@ -1,11 +1,11 @@
 import Router from 'express'
-import PostController from './postController.js'
+import PostService from './postController.js'
 const router = new Router()
 
-router.get('/posts', PostController.getAll)
-router.get('/posts/:id', PostController.getOne)
-router.post('/posts', PostController.create)
-router.put('/posts', PostController.update)
-router.delete('/posts/:id', PostController.delete)
+router.get('/posts', PostService.getAll)
+router.get('/posts/:id', PostService.getOne)
+router.post('/posts', PostService.create)
+router.put('/posts', PostService.update)
+router.delete('/posts/:id', PostService.delete)
 
 export default router
